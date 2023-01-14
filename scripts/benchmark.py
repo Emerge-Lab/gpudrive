b422e4459d0b6903ab294d9d69ac233aa2556cd3
Brennan Shacklett <bpshacklett@gmail.com>
2023-01-13 19:58:02 -0800

    Update madrona, add image dumping option to benchmark.py

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -3,11 +3,16 @@ import gpu_hideseek_python
 import torch
 import sys
 import time
+import PIL
+import PIL.Image
 
 num_worlds = int(sys.argv[1])
 num_steps = int(sys.argv[2])
 entities_per_world = int(sys.argv[3])
 reset_chance = float(sys.argv[4])
+
+render_width = 30 
+render_height = 1 
 
 sim = gpu_hideseek_python.HideAndSeekSimulator(
         exec_mode = gpu_hideseek_python.ExecMode.CUDA,
@@ -15,8 +20,8 @@ sim = gpu_hideseek_python.HideAndSeekSimulator(
         num_worlds = num_worlds,
         min_entities_per_world = entities_per_world,
         max_entities_per_world = entities_per_world,
-        render_width = 30,
-        render_height = 1,
+        render_width = render_width,
+        render_height = render_height,
         debug_compile = False,
 )
 
@@ -35,7 +40,22 @@ reset_rand = torch.zeros_like(resets[:, 0], dtype=torch.float32)
 
 resets[:, 0] = 1
 resets[:, 1] = 3
-resets[:, 2] = 3
+resets[:, 2] = 2
+
+def dump_obs(dump_dir, step_idx):
+    N = rgb_observations.shape[0]
+    A = rgb_observations.shape[1]
+
+    num_wide = min(64, N * A)
+
+    reshaped = rgb_observations.reshape(N * A // num_wide, num_wide, *rgb_observations.shape[2:])
+    grid = reshaped.permute(0, 2, 1, 3, 4)
+
+    grid = grid.reshape(N * A // num_wide * render_height, num_wide * render_width, 4)
+    grid = grid.type(torch.uint8).cpu().numpy()
+
+    img = PIL.Image.fromarray(grid)
+    img.save(f"{dump_dir}/{step_idx}.png", format="PNG")
 
 sim.step()
 
@@ -48,6 +68,9 @@ for i in range(num_steps):
     resets[:, 0].copy_(reset_cond)
 
     actions[..., 0:2] = torch.randint_like(actions[..., 0:2], -5, 5)
+
+    if len(sys.argv) > 5:
+        dump_obs(sys.argv[5], i)
 
 end = time.time()
 
