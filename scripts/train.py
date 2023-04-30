f15176208a19586faed2e8ea2af3b49ec9d7ae30
Brennan Shacklett <bpshacklett@gmail.com>
2023-04-30 15:47:30 -0700

    Update train script

--- scripts/train.py
+++ scripts/train.py
@@ -1,5 +1,6 @@
 import gpu_hideseek_python
 import madrona_learn
+from madrona_learn.model import SharedActorCritic, SmallMLP
 import torch
 import argparse
 import math
@@ -13,11 +14,12 @@ arg_parser.add_argument('--lr', type=float, default=1e-3)
 arg_parser.add_argument('--gamma', type=float, default=0.99)
 arg_parser.add_argument('--steps-per-update', type=int, default=100)
 arg_parser.add_argument('--gpu-id', type=int, default=0)
+arg_parser.add_argument('--cpu-sim', action='store_true')
 
 args = arg_parser.parse_args()
 
 sim = gpu_hideseek_python.HideAndSeekSimulator(
-        exec_mode = gpu_hideseek_python.ExecMode.CUDA,
+        exec_mode = gpu_hideseek_python.ExecMode.CPU if args.cpu_sim else gpu_hideseek_python.ExecMode.CUDA,
         gpu_id = args.gpu_id,
         num_worlds = args.num_worlds,
         min_entities_per_world = 0,
@@ -25,6 +27,13 @@ sim = gpu_hideseek_python.HideAndSeekSimulator(
         render_width = 0,
         render_height = 0,
 )
+
+if torch.cuda.is_available():
+    dev = torch.device(f'cuda:{args.gpu_id}')
+elif torch.backends.mps.is_available() and False:
+    dev = torch.device('mps')
+else:
+    dev = torch.device('cpu')
 
 def setup_obs():
     prep_counter_tensor = sim.prep_counter_tensor().to_torch()
@@ -47,41 +56,47 @@ def setup_obs():
     num_agent_data_features = math.prod(agent_data_tensor.shape[1:])
     num_box_data_features = math.prod(box_data_tensor.shape[1:])
     num_ramp_data_features = math.prod(ramp_data_tensor.shape[1:])
+
+    if dev.type == 'cuda':
+        conv_args = {
+                'dtype': torch.float16,
+                'non_blocking': True,
+            }
+    else:
+        conv_args = {
+                'dtype': torch.float32,
+            }
     
     def process_obs(prep_counter, agent_type, agent_data, box_data, ramp_data,
                     agent_mask, box_mask, ramp_mask):
         return torch.cat([
-                prep_counter.half(),
-                agent_type.half(),
-                (agent_data.half() * agent_mask.half()).view(
+                prep_counter.to(**conv_args),
+                agent_type.to(**conv_args),
+                (agent_data * agent_mask).to(**conv_args).view(
                     -1, num_agent_data_features),
-                (box_data.half() * box_mask.half()).view(
+                (box_data * box_mask).to(**conv_args).view(
                     -1, num_box_data_features),
-                (ramp_data.half() * ramp_mask.half()).view(
+                (ramp_data * ramp_mask).to(**conv_args).view(
                     -1, num_ramp_data_features)
             ], dim=1)
 
-    num_obs_features = prep_counter_tensor.shape[1] +
+    num_obs_features = prep_counter_tensor.shape[1] + \
         agent_type_tensor.shape[1] + \
         num_agent_data_features + num_box_data_features + \
         num_ramp_data_features
 
     return obs_tensors, process_obs, num_obs_features
 
-if torch.cuda.is_available():
-    dev = torch.device(f'cuda:{args.gpu_id}')
-elif torch.backends.mps.is_available():
-    dev = torch.device('mps')
-else:
-    dev = torch.device('cpu')
-
 obs_tensors, process_obs_cb, num_obs_features = setup_obs()
 
-policy = madrona_learn.model.ActorCritic()
+policy = SharedActorCritic(
+    process_obs_fn = process_obs_cb,
+    core = SmallMLP(num_obs_features, 512),
+    actor = SharedActorCritic.DefaultDiscreteActor(512, 3),
+    critic = SharedActorCritic.DefaultCritic(512))
 
 madrona_learn.train(madrona_learn.SimConfig(
                         step = lambda: sim.step(),
-                        process_obs = process_obs_cb,
                         obs = obs_tensors,
                         actions = sim.action_tensor().to_torch(),
                         rewards = sim.reward_tensor().to_torch(),
@@ -91,6 +106,7 @@ madrona_learn.train(madrona_learn.SimConfig(
                         gamma = args.gamma,
                         lr = args.lr,
                         steps_per_update = args.steps_per_update),
+                    policy,
                     dev = dev)
 
 del sim
