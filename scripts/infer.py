39ce39fe5d5bef7a4926906f8b10269df4ba0257
Brennan Shacklett <bpshacklett@gmail.com>
2023-07-23 23:18:22 -0700

    Fix infer.py

--- scripts/infer.py
+++ scripts/infer.py
@@ -47,7 +47,7 @@ dones = sim.done_tensor().to_torch()
 rewards = sim.reward_tensor().to_torch()
 
 # Flatten N, A, ... tensors to N * A, ...
-actions = actions.view(-1, *action.shape[2:])
+actions = actions.view(-1, *actions.shape[2:])
 dones  = dones.view(-1, *dones.shape[2:])
 rewards = rewards.view(-1, *rewards.shape[2:])
 
@@ -55,7 +55,7 @@ cur_rnn_states = []
 
 for shape in policy.recurrent_cfg.shapes:
     cur_rnn_states.append(torch.zeros(
-        *shape[0:2], actions.shape[0] * actions.shape[1], shape[2], dtype=torch.float32, device=torch.device('cpu')))
+        *shape[0:2], actions.shape[0], shape[2], dtype=torch.float32, device=torch.device('cpu')))
 
 action_log = open('/tmp/actions', 'wb')
 
