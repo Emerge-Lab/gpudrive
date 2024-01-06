e23b797650e4fca5f0b0f54516b8cbe9f01c957a
Saman Kazemkhani <skazemkhani@gmail.com>
2024-01-05 16:28:09 -0800

    Delete doorObservationTensor() declaration

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -40,8 +40,6 @@ NB_MODULE(madrona_escape_room, m) {
         .def("partner_observations_tensor", &Manager::partnerObservationsTensor)
         .def("room_entity_observations_tensor",
              &Manager::roomEntityObservationsTensor)
-        .def("door_observation_tensor",
-             &Manager::doorObservationTensor)
         .def("lidar_tensor", &Manager::lidarTensor)
         .def("steps_remaining_tensor", &Manager::stepsRemainingTensor)
     ;
