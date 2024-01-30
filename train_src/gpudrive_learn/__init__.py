6c0fddce4bd0adabce0cbc83be681ac07e981262
Aarav Pandya <ap7641@nyu.edu>
2024-01-30 20:46:51 +0530

    Implement python bindings (#26)
    * Init validation branch with configurable parameters
    
    * fix segFaults
    
    * Fix Bicycle Test
    
    * refactoring code for better readability
    
    * Fix bicyclemodel tests
    
    * Add test for road objects
    
    * Hook the threshold param in level_gen
    
    * Correct the paths
    
    * Change the jsonPath param
    
    * hard code number of road segments
    
    * Pass init objects by reference
    
    * Remove numAgents and numRoadSegments from Params
    
    * Correct test file
    
    * Hook up params with parsing
    
    * Fix mean and add proper road reduction algo
    
    * Working tests
    
    * Change paths
    
    * Cleanup
    
    * Init rewards
    
    * Copy params to CUDA
    
    * Init Done system
    
    * Init pybinds
    
    * Make changes to accomodate directory changes
    
    * remove pathToDirectory
    
    * Fix incorrect merge
    
    * Cleanup merge issues
    
    * Fix merge issues
    
    * Fix main merge
    
    * Fix assert

--- train_src/gpudrive_learn/__init__.py
+++ train_src/gpudrive_learn/__init__.py
@@ -1,15 +1,15 @@
-from madrona_escape_room_learn.train import train
-from madrona_escape_room_learn.learning_state import LearningState
-from madrona_escape_room_learn.cfg import TrainConfig, PPOConfig, SimInterface
-from madrona_escape_room_learn.action import DiscreteActionDistributions
-from madrona_escape_room_learn.actor_critic import (
+from gpudrive_learn.train import train
+from gpudrive_learn.learning_state import LearningState
+from gpudrive_learn.cfg import TrainConfig, PPOConfig, SimInterface
+from gpudrive_learn.action import DiscreteActionDistributions
+from gpudrive_learn.actor_critic import (
         ActorCritic, DiscreteActor, Critic,
         BackboneEncoder, RecurrentBackboneEncoder,
         Backbone, BackboneShared, BackboneSeparate,
     )
-from madrona_escape_room_learn.profile import profile
-import madrona_escape_room_learn.models
-import madrona_escape_room_learn.rnn
+from gpudrive_learn.profile import profile
+import gpudrive_learn.models
+import gpudrive_learn.rnn
 
 __all__ = [
         "train", "LearningState", "models", "rnn",
