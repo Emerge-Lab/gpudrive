1f7f6834823fb4f836d1b798405cbfa12604f880
Aarav Pandya <ap7641@nyu.edu>
2024-03-13 15:26:03 -0400

    realistic consts

--- src/consts.hpp
+++ src/consts.hpp
@@ -8,8 +8,8 @@ namespace gpudrive {
 
 namespace consts {
 
-inline constexpr madrona::CountT kMaxAgentCount = 50;
-inline constexpr madrona::CountT kMaxRoadEntityCount = 1000;
+inline constexpr madrona::CountT kMaxAgentCount = 100;
+inline constexpr madrona::CountT kMaxRoadEntityCount = 1600;
 
 // Various world / entity size parameters
 inline constexpr float worldLength = 40.f;
