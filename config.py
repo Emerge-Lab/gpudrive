e00808cd73b03c53c689fd514c872885ef3bf966
Aarav Pandya <ap7641@nyu.edu>
2024-03-14 13:59:16 -0400

    Wire config to get constants

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
 import re
 import os
-
+import yaml
 from scripts.sim_utils.create import SimCreator
 import build
+
+
+def get_constants():
+    fullpath = os.path.join(os.path.dirname(__file__), "config.yml")
+    sim = SimCreator(fullpath)
+    consts = sim.shape_tensor().to_torch().flatten().tolist()
+    print(consts)
+    return consts
 
 def update_constants(filepath, new_max_agent_count, new_max_road_entity_count):
     with open(filepath, 'r') as file:
@@ -25,7 +33,8 @@ def update_constants(filepath, new_max_agent_count, new_max_road_entity_count):
     build.build()
 
 def main():
-    print("Hello World!")
+    new_max_agent_count, new_max_road_entity_count = get_constants()
+    update_constants("src/consts.hpp", new_max_agent_count, new_max_road_entity_count+1)
 
 if __name__ == "__main__":
     update_constants("src/consts.hpp", 200, 2000)
