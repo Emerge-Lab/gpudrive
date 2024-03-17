98c0f21d1de5ed99de42e095481aa71859d6d7fc
Daphne Cornelisse <cor.daphne@gmail.com>
2024-03-16 21:10:38 -0400

    Render bug fixes

--- examples/pygame_render_test.py
+++ examples/pygame_render_test.py
@@ -17,11 +17,15 @@ def create_rgb_array(x_position):
     return np.transpose(np.array(pygame.surfarray.pixels3d(surf)), axes=(1, 0, 2))
 
 # Generate images for each timestep
+n_steps = 10
 frames = []
-for i in range(10):
+for i in range(n_steps):
     x_position = 50 + i*20  # Move the box to the right by 20 pixels each timestep
     rgb_array = create_rgb_array(x_position)
-    frames.append(rgb_array.astype('uint8'))
+    frames.append(rgb_array.T)
+
+# (n_steps, 3, 800, 600)
+print(np.array(frames).shape)
 
 # Log the series of images as a video to wandb
 wandb.log({"moving_box": wandb.Video(np.array(frames), fps=4, format="gif")})
