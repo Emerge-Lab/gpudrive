b118f622ffd8fb19e4029086e79ac208c0446b78
Aarav Pandya <ap7641@nyu.edu>
2024-03-19 20:01:32 -0400

    Save progress

--- scripts/plot_utils/plot_bench.py
+++ scripts/plot_utils/plot_bench.py
@@ -66,11 +66,39 @@ def plot_afps_vs_agents(results_path: str, output_path: str):
     plt.tight_layout()
     plt.savefig(output_path)
 
+def plot_randomized(results_path: str, output_path: str):
+    benchmark_data = pd.read_csv(results_path)
+    #select columns
+    benchmark_data = benchmark_data[['num_envs', 'useful_afps', 'actual_afps']]
+    benchmark_data = benchmark_data.groupby('num_envs').mean().reset_index()
+
+    # Using a different color palette and context
+    sns.set_theme(style="darkgrid", palette="muted", context="talk")
+
+    # Create a more refined and visually appealing plot
+    plt.figure(figsize=(12, 7))
+
+    # Create the enhanced plot
+    sns.lineplot(data=benchmark_data, x='num_envs', y='actual_afps', label='Actual AFPS')
+    sns.lineplot(data=benchmark_data, x='num_envs', y='useful_afps', label='Useful AFPS')
+
+    plt.title('AFPS vs Number of Environments (Randomized Sampling)', fontsize=20)
+    plt.xlabel('Number of Environments', fontsize=16)
+    plt.ylabel('AFPS (Agent adjusted Frames Per Second)', fontsize=16)
+
+    plt.grid(True)
+
+    plt.legend(title='AFPS Trend', title_fontsize='13', fontsize='12', loc='upper left')
+
+    plt.tight_layout()
+    plt.savefig(output_path)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='GPUDrive Plotting tool for benchmarking results')
-    parser.add_argument('--benchmarkResults', type=str, help='Path to the benchmark results', default='benchmark_results_binned.csv', required=False)
+    parser.add_argument('--benchmarkResults', type=str, help='Path to the benchmark results', default='benchmark_results.csv', required=False)
     parser.add_argument('--benchmarkType', type=str, help='Type of benchmarking results', default='afps', required=False)
     parser.add_argument('--outputPath', type=str, help='Path to save the plot', default='benchmark_plot.png', required=False)
     args = parser.parse_args()
     # plot_afps_curves(args.benchmarkResults, args.outputPath)
-    plot_afps_vs_agents(args.benchmarkResults, args.outputPath)
+    # plot_afps_vs_agents(args.benchmarkResults, args.outputPath)
+    plot_randomized(args.benchmarkResults, args.outputPath)
