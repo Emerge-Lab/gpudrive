587ba902de74391f552833b060df58145225cd73
Daphne Cornelisse <cor.daphne@gmail.com>
2024-04-01 15:30:29 -0400

    Add support for multi-agent PPO

--- experiments/run_ppo_sb3.py
+++ experiments/run_ppo_sb3.py
@@ -9,17 +9,17 @@ from algorithms.ppo.sb3.callbacks import MultiAgentCallback
 from algorithms.ppo.sb3.mappo import MAPPO
 
 if __name__ == "__main__":
-    
+
     # Make SB3-compatible environment
     env = SB3MultiAgentEnv(
-        num_worlds=1, 
-        max_cont_agents=1, 
-        data_dir='waymo_data', 
-        device='cuda', 
-        auto_reset=True
+        num_worlds=2,
+        max_cont_agents=2,
+        data_dir="waymo_data",
+        device="cuda",
+        auto_reset=True,
     )
-    
-    # Initialize wandb    
+
+    # Initialize wandb
     wandb.login()
     run = wandb.init(
         project="gpu_drive",
@@ -27,24 +27,26 @@ if __name__ == "__main__":
         sync_tensorboard=True,
     )
     run_id = run.id
-    
+
     # Initialize custom callback
     custom_callback = MultiAgentCallback(
         wandb_run=run if run_id is not None else None,
     )
-    
-    model = MAPPO(      
-        policy="MlpPolicy", # Policy type
-        n_steps=2048, # Number of steps per rollout
-        batch_size=256, # Minibatch size
-        env=env, # Our wrapped environment
-        seed=42, # Always seed for reproducibility
+
+    model = MAPPO(
+        policy="MlpPolicy",  # Policy type
+        n_steps=100,  # Number of steps per rollout
+        batch_size=100,  # Minibatch size
+        env=env,  # Our wrapped environment
+        seed=42,  # Always seed for reproducibility
         verbose=1,
-        tensorboard_log=f"runs/{run_id}" if run_id is not None else None, # Sync with wandb
+        tensorboard_log=f"runs/{run_id}"
+        if run_id is not None
+        else None,  # Sync with wandb
     )
-    
+
     # Learn
     model.learn(
-        total_timesteps=2_000_000,
+        total_timesteps=1000,
         callback=custom_callback,
     )
