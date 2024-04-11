89c3e91d6358c15eaefc64991b7fd61495050e7a
SamanKazemkhani <skazemkhani@gmail.com>
2024-04-11 14:46:03 -0400

    Export valid state

--- src/types.hpp
+++ src/types.hpp
@@ -174,6 +174,15 @@ struct AbsoluteSelfObservation {
     Goal goal;
 };
 
+enum class Validity : int32_t {
+    Invalid = 0,
+    Valid = 1
+};
+
+struct ValidState {
+    Validity valid[consts::kMaxAgentCount];
+};
+
 /* ECS Archetypes for the game */
 
 // There are 2 Agents in the environment trying to get to the destination
@@ -201,6 +210,7 @@ struct Agent : public madrona::Archetype<
     Goal,
     Trajectory,
     ControlledState,
+
     // Input
     Action,
 
@@ -212,7 +222,6 @@ struct Agent : public madrona::Archetype<
     Lidar,
     StepsRemaining,
     
-
     // Reward, episode termination
     Reward,
     Done,
