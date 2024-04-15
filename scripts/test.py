3ae73822937142026e529136fb0c938152a67309
Aarav Pandya <ap7641@nyu.edu>
2024-04-15 17:28:45 -0400

    Add scale to road observations (#83)

--- scripts/test.py
+++ scripts/test.py
@@ -2,21 +2,21 @@ import gpudrive
 import torch
 
 # Create an instance of RewardParams
-reward_params = gpudrive_.RewardParams()
-reward_params.rewardType = gpudrive_.RewardType.DistanceBased  # Or any other value from the enum
+reward_params = gpudrive.RewardParams()
+reward_params.rewardType = gpudrive.RewardType.DistanceBased  # Or any other value from the enum
 reward_params.distanceToGoalThreshold = 1.0  # Set appropriate values
 reward_params.distanceToExpertThreshold = 1.0  # Set appropriate values
 
 # Create an instance of Parameters
-params = gpudrive_.Parameters()
+params = gpudrive.Parameters()
 params.polylineReductionThreshold = 0.5  # Set appropriate value
 params.observationRadius = 10.0  # Set appropriate value
 params.collisionBehaviour = gpudrive.CollisionBehaviour.Ignore  # Set appropriate value
 params.rewardParams = reward_params  # Set the rewardParams attribute to the instance created above
 
 # Now use the 'params' instance when creating SimManager
-sim = gpudrive_.SimManager(
-    exec_mode=gpudrive_.madrona.ExecMode.CUDA,
+sim = gpudrive.SimManager(
+    exec_mode=gpudrive.madrona.ExecMode.CPU,
     gpu_id=0,
     num_worlds=1,
     auto_reset=True,
@@ -25,4 +25,4 @@ sim = gpudrive_.SimManager(
 )
 
 sim.step()
-print(sim.done_tensor().to_torch())
+print(sim.map_observation_tensor().to_torch().shape)
