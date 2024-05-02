d8128b9853e080fd66ab055006a65040f34e73f8
Daphne Cornelisse <cor.daphne@gmail.com>
2024-05-02 12:36:43 -0400

    Exper eval script.

--- utils/eval.py
+++ utils/eval.py
@@ -6,26 +6,9 @@ from tqdm import tqdm
 from pygpudrive.env.base_environment import Env
 from pygpudrive.env.config import EnvConfig
 
-
-def run_episode(env, eval_mode, policy=None):
-    """Run an episode."""
-
-    # Reset environment
-    obs = env.reset()
-
-    for _ in range(env.steps_remaining):
-
-        # Take action
-        action = select_action(obs, env, eval_mode, policy)
-
-        # Step environment
-        obs, reward, done, info = env.step(action)
-
-    # Return statistics
-    episode_stats = info.sum(axis=1).squeeze().tolist()
-    episode_stats.append(0)  # TODO: num_valid_agents
-
-    return torch.Tensor(episode_stats)
+# Constansts
+VEH_TYPE_ID = 7
+TIMESTEPS = 90
 
 
 def select_action(obs, env, eval_mode, policy=None):
@@ -43,6 +26,35 @@ def select_action(obs, env, eval_mode, policy=None):
         )
 
     return action
+
+
+def run_episode(env, eval_mode, metrics, policy=None):
+    """Run an episode."""
+
+    episode_stats = torch.zeros((env.num_sims, len(metrics) + 1))
+
+    # Reset environment
+    obs = env.reset()
+
+    for _ in range(TIMESTEPS):
+
+        # Take action
+        action = select_action(obs, env, eval_mode, policy)
+
+        # Step environment
+        obs, reward, done, info = env.step(action)
+
+    # Use type to idenfify vehicles
+    valid_veh_mask = info[:, :, 4] == VEH_TYPE_ID
+
+    # Return episode stats
+    for world_idx in range(env.num_sims):
+        episode_stats[world_idx, :4] = info[world_idx, :, :][
+            valid_veh_mask[world_idx, :]
+        ][:, :4].sum(axis=0)
+        episode_stats[world_idx, 4] = valid_veh_mask[world_idx, :].sum().item()
+
+    return episode_stats
 
 
 def evaluate_policy(
@@ -81,18 +93,23 @@ def evaluate_policy(
     )
 
     # Storage
-    episode_stats = torch.zeros((num_episodes, len(metrics) + 1))
+    episode_stats = torch.zeros((num_episodes, num_worlds, len(metrics) + 1))
 
     pbar = tqdm(range(num_episodes), colour="green")
     for episode_i in pbar:
-        episode_stats[episode_i, :] = run_episode(env, eval_mode)
+        episode_stats[episode_i, :] = run_episode(
+            env=env,
+            eval_mode=eval_mode,
+            metrics=metrics,
+        )
+
         pbar.set_description(
             f"Evaluating in {eval_mode} mode for {num_episodes} episodes"
         )
 
     # Make dataframe
     df_performance = pd.DataFrame(
-        episode_stats,
+        episode_stats.flatten(start_dim=0, end_dim=1),
         columns=metrics + ["num_valid_agents"],
     )
 
@@ -109,4 +126,6 @@ if __name__ == "__main__":
     df_eval = evaluate_policy(
         eval_mode="expert-teleport",
         data_dir="formatted_json_v2_no_tl_train",
+        num_episodes=1,
+        num_worlds=5,
     )
