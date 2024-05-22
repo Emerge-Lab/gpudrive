5c5a6f5df43b3a29e4d1f5ba00c50484eb455a0a
Daphne Cornelisse <cor.daphne@gmail.com>
2024-05-22 18:09:33 -0400

    Name change fix.

--- algorithms/sb3/ppo/ippo.py
+++ algorithms/sb3/ppo/ippo.py
@@ -10,7 +10,7 @@ from stable_baselines3.common.vec_env import VecEnv
 from torch import nn
 
 # Import masked rollout buffer class
-from algorithms.ppo.sb3.rollout_buffer import MaskedRolloutBuffer
+from algorithms.sb3.rollout_buffer import MaskedRolloutBuffer
 
 # From stable baselines
 def explained_variance(
