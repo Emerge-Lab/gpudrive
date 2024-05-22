034124e9f40d0cb987aa2e226b20e191b55df340
Aarav Pandya <ap7641@nyu.edu>
2024-05-21 22:25:13 -0400

    Reduced consts to use less memory

--- src/consts.hpp
+++ src/consts.hpp
@@ -8,9 +8,9 @@ namespace gpudrive {
 
 namespace consts {
 
-inline constexpr madrona::CountT kMaxAgentCount = 128;
-inline constexpr madrona::CountT kMaxRoadEntityCount = 6000;
-inline constexpr madrona::CountT kMaxAgentMapObservationsCount = 6000;
+inline constexpr madrona::CountT kMaxAgentCount = 40;
+inline constexpr madrona::CountT kMaxRoadEntityCount = 4500;
+inline constexpr madrona::CountT kMaxAgentMapObservationsCount = 4500;
 
 // Various world / entity size parameters
 inline constexpr float worldLength = 40.f;
