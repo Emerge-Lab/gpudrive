c56073e98488bcde4e916abdfef88c464bf50f11
Daphne Cornelisse <33460159+daphnecor@users.noreply.github.com>
2024-05-31 16:09:38 -0400

    Env fixed and training config (#157)

--- baselines/ippo/run_ppo_sb3.py
+++ baselines/ippo/run_ppo_sb3.py
@@ -1,65 +1,50 @@
-import os
-
-import torch
 import wandb
+import pyrallis
 
-# Import the EnvConfig dataclass
 from pygpudrive.env.config import EnvConfig
-
-# Import env wrapper that makes gym env compatible with stable-baselines3
 from pygpudrive.env.wrappers.sb3_wrapper import SB3MultiAgentEnv
 
-from algorithms.sb3.callbacks import MultiAgentCallback
-
-# Import adapted PPO version
 from algorithms.sb3.ppo.ippo import IPPO
-from baselines.config import ExperimentConfig
-from networks.basic_ffn import FeedForwardPolicy, FFN
-from networks.perm_eq_late_fusion import LateFusionNet, LateFusionPolicy
+from algorithms.sb3.callbacks import MultiAgentCallback
+from baselines.ippo.config import ExperimentConfig
 
-torch.cuda.empty_cache()
 
-# os.environ["MADRONA_MWGPU_KERNEL_CACHE"] = "./gpudrive_cache"
+def train():
+    """Run PPO training with stable-baselines3."""
 
-if __name__ == "__main__":
+    exp_config = pyrallis.parse(config_class=ExperimentConfig)
 
     env_config = EnvConfig(
-        ego_state=True,
-        road_map_obs=True,
-        partner_obs=True,
-        norm_obs=True,
         road_obs_algorithm="k_nearest_roadpoints",
         sample_method="pad_n",
     )
 
-    exp_config = ExperimentConfig(
-        render=False,
-    )
-
-    # Make SB3-compatible environment
+    # MAKE SB3-COMPATIBLE ENVIRONMENT
     env = SB3MultiAgentEnv(
         config=env_config,
-        num_worlds=100,
-        max_cont_agents=128,
+        num_worlds=env_config.num_worlds,
+        max_cont_agents=env_config.num_controlled_vehicles,
         data_dir=exp_config.data_dir,
         device=exp_config.device,
     )
 
+    # INIT WANDB
     run_id = None
     if exp_config.use_wandb:
         run = wandb.init(
-            project="rl_bench",
-            group="render_test",
-            sync_tensorboard=True,
+            project=exp_config.project_name,
+            group=exp_config.group_name,
+            sync_tensorboard=exp_config.sync_tensorboard,
         )
         run_id = run.id
 
-    # Initialize custom callback
+    # CALLBACK
     custom_callback = MultiAgentCallback(
         config=exp_config,
         wandb_run=run if run_id is not None else None,
     )
 
+    # INITIALIZE IPPO
     model = IPPO(
         n_steps=exp_config.n_steps,
         batch_size=exp_config.batch_size,
@@ -70,11 +55,17 @@ if __name__ == "__main__":
         tensorboard_log=f"runs/{run_id}"
         if run_id is not None
         else None,  # Sync with wandb
-        mlp_class=LateFusionNet,
-        policy=LateFusionPolicy,  # TODO(ev) configure from config
+        mlp_class=exp_config.mlp_class,
+        policy=exp_config.policy,
+        gamma=exp_config.gamma,
+        gae_lambda=exp_config.gae_lambda,
+        vf_coef=exp_config.vf_coef,
+        clip_range=exp_config.clip_range,
+        env_config=env_config,
+        exp_config=exp_config,
     )
 
-    # Learn
+    # LEARN
     model.learn(
         total_timesteps=exp_config.total_timesteps,
         callback=custom_callback,
@@ -82,3 +73,7 @@ if __name__ == "__main__":
 
     run.finish()
     env.close()
+
+
+if __name__ == "__main__":
+    train()
