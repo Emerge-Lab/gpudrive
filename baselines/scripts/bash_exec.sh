b6072fe2a84df2bdc534f77e238a52aa2252c688
daphnecor <cor.daphne@gmail.com>
2024-06-03 11:31:55 -0400

    cluster

--- baselines/scripts/bash_exec.sh
+++ baselines/scripts/bash_exec.sh
@@ -2,8 +2,3 @@
 
 # Run PPO script
 /scratch/dc4971/.conda/gpudrive/bin/python baselines/ippo/run_sb3_ppo.py --seed=23
-
-# !/bin/sh
-
-# Run PPO script
-/scratch/dc4971/.conda/gpudrive/bin/python baselines/ippo/run_sb3_ppo.py --num_worlds=10 --seed=23
