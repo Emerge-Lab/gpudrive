481e824b45b5ea15a174b23b39905609a0e7748f
Aarav Pandya <ap7641@nyu.edu>
2024-06-12 17:11:26 -0400

    Waymax model (#132)
    * Init waymax changes
    
    * Working bicyclemodel using madrona types
    
    * Update tests
    
    * Compute inverse dynamics
    
    * Position can be ignored
    
    * Export expert trajectory
    
    * Calculate inverse actions in trajectory
    
    * Inline the functions
    
    * cleanup
    
    * Add tests
    
    * Working dynamics
    
    * Clip actions
    
    * set tests path
    
    * Change consts to pass the test
    
    * Relax the threshold a bit
    
    * Simplify the forward dynamics
    
    * Remove bicycle model struct
    
    * Merge conflicts
    
    * Remove autoreset from waymax test

--- src/headless.cpp
+++ src/headless.cpp
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
         .execMode = exec_mode,
         .gpuID = 0,
         .numWorlds = (uint32_t)num_worlds,
-        .jsonPath = "../maps.16",
+        .jsonPath = "tests/testJsons",
         .params = {
             .polylineReductionThreshold = 1.0,
             .observationRadius = 100.0,
