7afe4e81d2a59e87791ef91ac3768b54ddc61118
Daphne Cornelisse <cor.daphne@gmail.com>
2024-07-11 16:33:01 -0400

    Update data generation code

--- algorithms/il/data_generation.py
+++ algorithms/il/data_generation.py
@@ -4,61 +4,104 @@ from pygpudrive.env.config import EnvConfig, RenderConfig
 from pygpudrive.env.env_torch import GPUDriveTorchEnv
 
 
-if __name__ == "__main__":
+def generate_state_action_pairs(
+    env_config,
+    render_config,
+    max_num_objects,
+    num_worlds,
+    data_dir,
+    device,
+    discretize_actions=False,
+    use_heading=False,
+):
+    """Generate pairs of states and actions from the Waymo Open Dataset.
 
-    # Constants
-    TOTAL_STEPS = 10
-    MAX_NUM_OBJECTS = 128
-    NUM_WORLDS = 10
+    Args:
+        env_config (EnvConfig): Environment configurations.
+        render_config (RenderConfig): Render configurations.
+        max_num_objects (int): Maximum controllable agents per world.
+        num_worlds (int): Number of worlds (envs).
+        data_dir (str): Path to folder with scenarios (Waymo Open Motion Dataset)
+        device (str): Where to run the simulation (cpu or cuda).
+        discretize_actions (bool): Whether to discretize the expert actions.
+        use_heading (bool): Whether to use heading information in the expert actions.
 
-    render_config = RenderConfig()
+    Returns:
+        expert_actions: Expert actions for the controlled agents.
+        obs_tensor: Expert observations for the controlled agents.
+    """
 
-    # Make environment with Waymax model
-    env_config = EnvConfig(use_waymax_model=True, sample_method="pad_n")
-
+    # Make environment with chosen scenarios
     env = GPUDriveTorchEnv(
         config=env_config,
-        num_worlds=NUM_WORLDS,
-        max_cont_agents=MAX_NUM_OBJECTS,
-        data_dir="example_data",
-        device="cuda",
         render_config=render_config,
+        num_worlds=num_worlds,
+        max_cont_agents=max_num_objects,
+        data_dir=data_dir,
+        device=device,
     )
 
-    # Reset
+    # Reset the environment
     obs = env.reset()
 
-    # Obtain expert actions for full trajectory in all worlds
+    # Get expert actions for full trajectory in all worlds
     expert_actions = env.get_expert_actions()
-
-    # TODO: Discretize expert actions
-
-    # # Extract expert actions for the controlled (valid) agents
-    # expert_actions = expert_actions[env.cont_agent_mask, :, :]
+    if discretize_actions:
+        # TODO(dc): Implement discretization
+        expert_actions = env.discretize_actions(expert_actions)
 
     # Storage
-    obs_tensor = torch.zeros(
-        (
-            env.num_valid_controlled_agents_across_worlds,
-            env.episode_len,
-            env.observation_space.shape[0],
-        ),
-    )
+    expert_observations_lst = []
+    expert_actions_lst = []
 
-    # Obtain the expert observations by stepping through the env
-    # using the expert actions
-    # TODO(dc) - rmv obs, actions from dead agents
+    # Initialize dead agent mask
+    dead_agent_mask = ~env.cont_agent_mask.clone()
+
     for time_step in range(env.episode_len):
 
-        # Step using the expert actions
+        # Step the environment with inferred expert actions
         env.step_dynamics(expert_actions[:, :, time_step, :])
 
         next_obs = env.get_obs()
+        dones = env.get_dones()
 
-        # Append (expert_actions, expert_obs) to the tensor
-        obs_tensor[:, time_step, :] = next_obs[env.cont_agent_mask, :]
+        # Unpack and store (obs, action) pairs for controlled agents
+        expert_observations_lst.append(obs[~dead_agent_mask, :])
+        expert_actions_lst.append(
+            expert_actions[~dead_agent_mask][:, time_step, :]
+        )
+
+        # Update
+        obs = next_obs
+        dead_agent_mask = torch.logical_or(dead_agent_mask, dones)
+
+        if (dead_agent_mask == True).all():
+            break
+
+    # Convert lists to tensors
+    # Flatten the lists
+    flattened_expert_obs = torch.cat(expert_observations_lst, dim=0)
+    flattened_expert_actions = torch.cat(expert_actions_lst, dim=0)
+
+    return flattened_expert_obs, flattened_expert_actions
+
+
+if __name__ == "__main__":
+
+    # Set the environment and render configurations
+    env_config = EnvConfig(sample_method="pad_n", use_bicycle_model=True)
+    render_config = RenderConfig()
+
+    # Generate expert actions and observations
+    expert_obs, expert_actions = generate_state_action_pairs(
+        env_config=env_config,
+        render_config=render_config,
+        max_num_objects=128,
+        num_worlds=10,
+        data_dir="example_data",
+        device="cuda",
+    )
 
     # Save the expert actions and observations
-    # ...
-    # torch.save(expert_actions[env.cont_agent_mask, :, :], "expert_actions.pt")
-    # torch.save(obs_tensor, "expert_obs.pt")
+    torch.save(expert_actions, "expert_actions.pt")
+    torch.save(expert_obs, "expert_obs.pt")
