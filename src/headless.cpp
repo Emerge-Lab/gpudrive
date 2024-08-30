7572e6579c303603f47262f7820e6b38c8d387a1
Aarav Pandya <ap7641@nyu.edu>
2024-08-30 16:05:12 -0400

    Run headless on expert mode

--- src/headless.cpp
+++ src/headless.cpp
@@ -58,7 +58,7 @@ int main(int argc, char *argv[])
                 .distanceToGoalThreshold = 0.5,
                 .distanceToExpertThreshold = 0.5
             },
-            .maxNumControlledVehicles = 2,
+            .maxNumControlledVehicles = 0,
         }
     });
 
