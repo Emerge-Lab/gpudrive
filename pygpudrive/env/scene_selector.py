e539a6b2a235994e83a2b86b7ca83ee836345a13
Daphne Cornelisse <cor.daphne@gmail.com>
2024-08-30 09:17:04 -0400

    Ensure that all files in the scene pool are actually traffic scenes

--- pygpudrive/env/scene_selector.py
+++ pygpudrive/env/scene_selector.py
@@ -11,7 +11,10 @@ def select_scenes(config):
         config.path
     ), "The data directory does not exist or is empty."
 
-    all_scenes = sorted(os.listdir(config.path))
+    all_scenes = sorted(os.listdir(config.path)) 
+    # Remove elements that are not tfrecord files (traffic scenes)
+    all_scenes = [scene for scene in all_scenes if scene.startswith("tfrecord")]
+    
     selected_scenes = None
     if not any(scene.startswith("tfrecord") for scene in all_scenes):
         raise ValueError(
@@ -43,13 +46,11 @@ def select_scenes(config):
             ), "K_UNIQUE_N discipline requires specifying positive value for K"
             selected_scenes = repeat_to_N(
                 random_sample(config.k_unique_scenes)
-            )
-
-    if not any(scene.startswith("tfrecord") for scene in selected_scenes):
+            ) 
+    if not any(scene.startswith("tfrecord") for scene in selected_scenes) or len(selected_scenes) == 0:
         raise ValueError(
             "The selected scenes do not contain traffic scenes. Something went wrong with the scene selection."
         )
-
     scene_paths = [
         os.path.join(os.path.abspath(config.path), selected_scene)
         for selected_scene in selected_scenes
