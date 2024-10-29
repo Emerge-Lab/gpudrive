a810e220996d37764446c4b6786b707886e5a0b2
Daphne Cornelisse <cor.daphne@gmail.com>
2024-10-28 21:13:31 -0400

    Integrate new polyline construction code

--- pygpudrive/env/env_torch.py
+++ pygpudrive/env/env_torch.py
@@ -425,7 +425,7 @@ class GPUDriveTorchEnv(GPUDriveGymEnv):
         )
 
         return obs_filtered
-    
+
     def advance_sim_with_log_playback(self, init_steps=0, render_init=False):
         """Advances the simulator by stepping the objects with the logged human trajectories.
 
@@ -460,7 +460,7 @@ class GPUDriveTorchEnv(GPUDriveGymEnv):
             agents_history[:, :, 0, :], _, _ = self.construct_agent_traj()
 
         self.init_frames = []
-        
+
         for time_step in range(init_steps):
             self.step_dynamics(
                 actions=self.log_playback_traj[:, :, time_step, :]
@@ -839,18 +839,19 @@ class GPUDriveTorchEnv(GPUDriveGymEnv):
 
 if __name__ == "__main__":
 
-    
     init_steps = 10
     MAX_NUM_OBJECTS = 32
     NUM_WORLDS = 1
 
     env_config = EnvConfig(
-        init_steps=init_steps,
-        return_vbd_data=True,
-        dynamics_model="state",
+        init_steps=init_steps,  # Warmup period
+        return_vbd_data=True,  # Use VBD
+        dynamics_model="state",  # Use state-based dynamics model
+        dist_to_goal_threshold=1e-5,  # Trick to make sure the agents don't disappear when they reach the goal
+        collision_behavior="ignore",  # Ignore collisions
     )
     render_config = RenderConfig()
-    scene_config = SceneConfig("data/processed/training", NUM_WORLDS)
+    scene_config = SceneConfig("data/processed/validation", NUM_WORLDS)
 
     # Make env
     env = GPUDriveTorchEnv(
@@ -864,7 +865,7 @@ if __name__ == "__main__":
     obs = env.reset()
 
     sample_batch = env.sample_batch
-    
+
     frames = []
 
     for t in range(TOTAL_STEPS):
