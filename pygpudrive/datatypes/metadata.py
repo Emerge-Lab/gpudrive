b8b776ed158c388910ca3ddb3b9de87e3ebc1f7e
kevin <kevinwinston184@gmail.com>
2024-12-06 11:29:23 -0500

    QOL and docstring

--- pygpudrive/datatypes/metadata.py
+++ pygpudrive/datatypes/metadata.py
@@ -8,7 +8,7 @@ class Metadata:
     Initialized from metadata_tensor (src/bindings). For details, see
     `MetaData` in src/types.hpp
     
-    Attributes (all masks are 0/1 int):
+    Attributes (all masks are 0/1 int of shape (NumWorlds, NumAgents)):
         sdc_mask: Whether agent is self-driving car.
         interested: IDs of agents interested (-1 otherwise).
         prediction_mask: Whether agent's trajectory needs to be predicted (WOSAC).
@@ -16,7 +16,7 @@ class Metadata:
     """
 
     def __init__(self, metadata_tensor: torch.Tensor):
-        """Initializes the ego state with an observation tensor."""
+        """Initializes the Metadata with the metadata tensor."""
         metadata_tensor = metadata_tensor.reshape(-1, 4, gpudrive.kMaxAgentCount)
         self.sdc_mask = metadata_tensor[:, 0, :]
         self.interested = metadata_tensor[:, 1, :]
@@ -27,8 +27,13 @@ class Metadata:
     def from_tensor(
         cls, metadata_tensor: gpudrive.madrona.Tensor, backend="torch"
     ):
-        """Creates an LocalEgoState from the agent_observation_tensor."""
+        """Creates a Metadata object from the metadata_tensor."""
         if backend == "torch":
             return cls(metadata_tensor.to_torch().clone())
         elif backend == "jax":
             raise NotImplementedError("JAX backend not implemented yet.")
+    
+    @property
+    def shape(self) -> tuple[int, ...]:
+        """Shape (num_worlds, num_agents) of each metadata mask."""
+        return self.sdc_mask.shape
