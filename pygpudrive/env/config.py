Correct implementation of masking with obs processing to speed up training loop

pygpudrive/env/config.py
@@ -60,10 +60,10 @@ class EnvConfig:
     # Action space settings (if discretized)
     # Classic or Invertible Bicycle dynamics model
     steer_actions: torch.Tensor = torch.round(
-        torch.linspace(-torch.pi, torch.pi, 41), decimals=3
+        torch.linspace(-torch.pi, torch.pi, 13), decimals=3  
     )
     accel_actions: torch.Tensor = torch.round(
-        torch.linspace(-4.0, 4.0, 17), decimals=3
+        torch.linspace(-4.0, 4.0, 7), decimals=3
     )
     head_tilt_actions: torch.Tensor = torch.Tensor([0])
 
@@ -116,10 +116,8 @@ class EnvConfig:
     num_lidar_samples: int = gpudrive.numLidarSamples
 
     # Initialization mode
-    init_mode: str = (
-        "all_non_trivial"  # Options: all_non_trivial, all_objects, all_valid
-    )
-
+    init_mode: str = "all_non_trivial" # Options: all_non_trivial, all_objects, all_valid
+    
 
 class SelectionDiscipline(Enum):
     """Enum for selecting scenes discipline in dataset configuration."""

pygpudrive/env/env_torch.py
@@ -65,7 +65,7 @@ class GPUDriveTorchEnv(GPUDriveGymEnv):
 
         # Setup action and observation spaces
         self.observation_space = Box(
-            low=-np.inf, high=np.inf, shape=(self.get_obs().shape[-1],)
+            low=-np.inf, high=np.inf, shape=(self.get_obs(self.cont_agent_mask).shape[-1],)
         )
         # self.single_observation_space = Box(
         #     low=-np.inf, high=np.inf,  shape=(self.observation_space.shape[-1],), dtype=np.float32
@@ -99,10 +99,10 @@ class GPUDriveTorchEnv(GPUDriveGymEnv):
             env_config=self.config,
         )
 
-    def reset(self):
+    def reset(self, mask=None):
         """Reset the worlds and return the initial observations."""
         self.sim.reset(list(range(self.num_worlds)))
-        return self.get_obs()
+        return self.get_obs(mask)
 
     def get_dones(self):
         return (
@@ -355,20 +355,23 @@ class GPUDriveTorchEnv(GPUDriveGymEnv):
         )
         return action_space
 
-    def _get_ego_state(self) -> torch.Tensor:
+    def _get_ego_state(self, mask) -> torch.Tensor:
         """Get the ego state.
         Returns:
-            Shape: (num_worlds, max_agents, num_features)
+            Shape: (num_worlds, controlled_agents, num_features)
         """
-        if self.config.ego_state:
-            ego_state = LocalEgoState.from_tensor(
-                self_obs_tensor=self.sim.self_observation_tensor(),
-                backend=self.backend,
-            )
-            if self.config.norm_obs:
-                ego_state.normalize()
+        if not self.config.ego_state:
+            return torch.Tensor().to(self.device)
+        
+        ego_state = LocalEgoState.from_tensor(
+            self_obs_tensor=self.sim.self_observation_tensor(),
+            backend=self.backend,
+            mask=mask
+        )  
+        if self.config.norm_obs:
+            ego_state.normalize()
 
-            return (
+        return (
                 torch.stack(
                     [
                         ego_state.speed,
@@ -379,178 +382,93 @@ class GPUDriveTorchEnv(GPUDriveGymEnv):
                         ego_state.is_collided,
                     ]
                 )
-                .permute(1, 2, 0)
+                .permute(1, 0)
                 .to(self.device)
             )
-        else:
-            return torch.Tensor().to(self.device)
 
-    def _get_partner_obs(self):
+    def _get_partner_obs(self, mask):
         """Get partner observations."""
-        if self.config.partner_obs:
-            partner_obs = PartnerObs.from_tensor(
-                partner_obs_tensor=self.sim.partner_observations_tensor(),
-                backend=self.backend,
-            )
-
-            if self.config.norm_obs:
-                partner_obs.normalize()
-                partner_obs.one_hot_encode_agent_types()
-
-            return (
-                torch.concat(
-                    [
-                        partner_obs.speed,
-                        partner_obs.rel_pos_x,
-                        partner_obs.rel_pos_y,
-                        partner_obs.orientation,
-                        partner_obs.vehicle_length,
-                        partner_obs.vehicle_width,
-                        # partner_obs.agent_type,
-                    ],
-                    dim=-1,
-                )
-                .flatten(start_dim=2)
-                .to(self.device)
-            )
-
-        else:
+        if not self.config.partner_obs:
             return torch.Tensor().to(self.device)
+        
+        partner_obs = PartnerObs.from_tensor(
+            partner_obs_tensor=self.sim.partner_observations_tensor(),
+            backend=self.backend,
+            mask=mask
+        )
 
-    def _get_road_map_obs(self):
+        if self.config.norm_obs:
+            partner_obs.normalize()
+            #partner_obs.one_hot_encode_agent_types()
+
+        return partner_obs.data.flatten(start_dim=1)
+
+    def _get_road_map_obs(self, mask):
         """Get road map observations."""
-        if self.config.road_map_obs:
-            roadgraph = LocalRoadGraphPoints.from_tensor(
-                local_roadgraph_tensor=self.sim.agent_roadmap_tensor(),
-                backend=self.backend,
-            )
-
-            if self.config.norm_obs:
-                roadgraph.normalize()
-                roadgraph.one_hot_encode_road_point_types()
-
-            return (
-                torch.cat(
-                    [
-                        roadgraph.x.unsqueeze(-1),
-                        roadgraph.y.unsqueeze(-1),
-                        roadgraph.segment_length.unsqueeze(-1),
-                        roadgraph.segment_width.unsqueeze(-1),
-                        roadgraph.segment_height.unsqueeze(-1),
-                        roadgraph.orientation.unsqueeze(-1),
-                        roadgraph.type,
-                    ],
-                    dim=-1,
-                )
-                .flatten(start_dim=2)
-                .to(self.device)
-            )
-
-        else:
+        if not self.config.road_map_obs:
             return torch.Tensor().to(self.device)
 
-    def _get_lidar_obs(self):
+        roadgraph = LocalRoadGraphPoints.from_tensor(
+            local_roadgraph_tensor=self.sim.agent_roadmap_tensor(),
+            backend=self.backend,
+            mask=mask
+        )
+
+        if self.config.norm_obs:
+            roadgraph.normalize()
+            roadgraph.one_hot_encode_road_point_types()
+            
+        return torch.cat([
+            roadgraph.data,
+            roadgraph.type,
+        ], dim=-1).flatten(start_dim=1)
+        
+
+    def _get_lidar_obs(self, mask):
         """Get lidar observations."""
-        if self.config.lidar_obs:
-            lidar = LidarObs.from_tensor(
-                lidar_tensor=self.sim.lidar_tensor(),
-                backend=self.backend,
-            )
-
-            return (
-                torch.cat(
-                    [
-                        lidar.agent_samples,
-                        lidar.road_edge_samples,
-                        lidar.road_line_samples,
-                    ],
-                    dim=-1,
-                )
-                .flatten(start_dim=2)
-                .to(self.device)
-            )
-        else:
+        if not self.config.lidar_obs:
             return torch.Tensor().to(self.device)
 
-    def get_obs(self):
+        lidar = LidarObs.from_tensor(
+            lidar_tensor=self.sim.lidar_tensor(),
+            backend=self.backend,
+        )
+
+        return [
+            lidar.agent_samples[mask],
+            lidar.road_edge_samples[mask],
+            lidar.road_line_samples[mask],
+        ]
+
+    def get_obs(self, mask=None):
         """Get observation: Combine different types of environment information into a single tensor.
 
         Returns:
             torch.Tensor: (num_worlds, max_agent_count, num_features)
         """
+        ego_states = self._get_ego_state(mask)
+        partner_observations = self._get_partner_obs(mask)
+        road_map_observations = self._get_road_map_obs(mask)
+        lidar_obs = self._get_lidar_obs(mask)
+  
+        #import pdb; pdb.set_trace()
 
-        ego_states = self._get_ego_state()
-
-        partner_observations = self._get_partner_obs()
-
-        road_map_observations = self._get_road_map_obs()
-
-        lidar_obs = self._get_lidar_obs()
-
-        obs_filtered = torch.cat(
+        obs = torch.cat(
             (
                 ego_states,
                 partner_observations,
                 road_map_observations,
-                lidar_obs,
             ),
             dim=-1,
         )
-
-        return obs_filtered
+        
+        return obs
 
     def get_controlled_agents_mask(self):
         """Get the control mask."""
         return (
             self.sim.controlled_state_tensor().to_torch().clone() == 1
         ).squeeze(axis=2)
-        
-    def remove_agents_by_id(self, perc_to_rmv_per_scene, remove_controlled_agents=True):
-        """Delete random agents in scenarios.
-        
-        Args:
-            perc_to_rmv_per_scene (float): Percentage of agents to remove per scene
-            remove_controlled_agents (bool): If True, removes controlled agents. If False, removes uncontrolled agents
-        """
-        # Obtain agent ids
-        agent_ids = LocalEgoState.from_tensor(
-            self_obs_tensor=self.sim.self_observation_tensor(),
-            backend='torch',
-            device=self.device
-        ).id
-        
-        # Choose the appropriate mask based on whether we're removing controlled or uncontrolled agents
-        if remove_controlled_agents:
-            agent_mask = self.cont_agent_mask
-        else:
-            # Create inverse mask for uncontrolled agents
-            agent_mask = ~self.cont_agent_mask
-        
-        for env_idx in range(self.num_worlds):
-            # Get all relevant agent IDs (controlled or uncontrolled) for the current environment
-            scene_agent_ids = agent_ids[env_idx, :][agent_mask[env_idx]].long()
-            
-            if scene_agent_ids.numel() > 0:  # Ensure there are agents to sample
-                # Determine the number of agents to sample (X% of the total agents)
-                num_to_sample = max(1, int(perc_to_rmv_per_scene * scene_agent_ids.size(0)))
-                
-                # Randomly sample agent IDs to remove using torch
-                sampled_indices = torch.randperm(scene_agent_ids.size(0))[:num_to_sample]
-                sampled_agent_ids = scene_agent_ids[sampled_indices]
-                
-                # Delete the sampled agents from the environment
-                self.sim.deleteAgents({env_idx: sampled_agent_ids.tolist()})
-        
-        # Reset controlled agent mask and visualizer
-        self.cont_agent_mask = self.get_controlled_agents_mask()
-        self.max_agent_count = self.cont_agent_mask.shape[1]
-        self.num_valid_controlled_agents_across_worlds = (
-            self.cont_agent_mask.sum().item()
-        )
-        
-        # Reset static scenario data for the visualizer
-        self.vis.initialize_static_scenario_data(self.cont_agent_mask)
-        
 
     def swap_data_batch(self, data_batch=None):
         """
@@ -647,28 +565,10 @@ class GPUDriveTorchEnv(GPUDriveGymEnv):
             log_trajectory.vel_xy,
             log_trajectory.yaw,
         )
-        
-    def get_env_filenames(self):
-        """Obtain the tfrecord filename for each world, mapping world indices to map names."""
-        
-        map_name_integers = self.sim.map_name_tensor().to_torch()
-        
-        filenames = {}
-        
-        # Iterate through the number of worlds
-        for i in range(self.num_worlds):
-            tensor = map_name_integers[i]
-            
-            # Convert ints to characters, ignoring zeros
-            map_name = ''.join([chr(i) for i in tensor.tolist() if i != 0])
-            
-            # Map the world index to the corresponding map name
-            filenames[i] = map_name
-        
-        return filenames
 
 
 if __name__ == "__main__":
+
     from pygpudrive.visualize.utils import img_from_fig
     import mediapy as media
 
@@ -678,7 +578,7 @@ if __name__ == "__main__":
 
     # Create data loader
     train_loader = SceneDataLoader(
-        root="data/processed/training",
+        root="/scratch/kj2676/gpudrive/data/processed/training",
         batch_size=data_config.batch_size,
         dataset_size=data_config.dataset_size,
         sample_with_replacement=True,
@@ -689,20 +589,16 @@ if __name__ == "__main__":
     env = GPUDriveTorchEnv(
         config=env_config,
         data_loader=train_loader,
-        max_cont_agents=128,  # Number of agents to control
+        max_cont_agents=64, # Number of agents to control
         device="cuda",
     )
-
-    print(f"dataset: {env.data_batch}")
-
-    print(
-        f"controlled agents mask [before reset]: {env.cont_agent_mask.sum()}"
-    )
-
+    
+    controlled_agent_mask = env.cont_agent_mask.clone()
+    
     # Rollout
-    obs = env.reset()
-
-    print(f"controlled agents mask: {env.cont_agent_mask.sum()}")
+    obs = env.reset(controlled_agent_mask)
+    
+    print(f"controlled agents mask: {controlled_agent_mask.sum()}")
 
     sim_frames = []
     agent_obs_frames = []
@@ -711,6 +607,7 @@ if __name__ == "__main__":
     # env.reset()
 
     expert_actions, _, _, _ = env.get_expert_actions()
+    
 
     env_idx = 0
 
@@ -718,7 +615,6 @@ if __name__ == "__main__":
         print(f"Step: {t}")
 
         # Step the environment
-        expert_actions, _, _, _ = env.get_expert_actions()
         env.step_dynamics(expert_actions[:, :, t, :])
 
         # if (t + 1) % 2 == 0:
@@ -729,7 +625,7 @@ if __name__ == "__main__":
         # sim_state[0].savefig(f"sim_state.png")   # Save the figure to a file
         # agent_obs_fig.savefig(f"agent_obs.png")  # Save the figure to a file
 
-        highlight_agent = torch.where(env.cont_agent_mask[env_idx, :])[0][
+        highlight_agent = torch.where(controlled_agent_mask[env_idx, :])[0][
             -1
         ].item()
 
@@ -753,7 +649,7 @@ if __name__ == "__main__":
         sim_frames.append(img_from_fig(sim_states[0]))
         agent_obs_frames.append(img_from_fig(agent_obs))
 
-        obs = env.get_obs()
+        obs = env.get_obs(controlled_agent_mask)
         reward = env.get_rewards()
         done = env.get_dones()
         info = env.get_infos()