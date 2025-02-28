67a69710ebef878c8580746cda9250aaf7293cb7
charliemolony <charliemolony59@gmail.com>
2025-02-28 15:32:01 -0500

    finishing tutorial 7

--- gpudrive/visualize/core.py
+++ gpudrive/visualize/core.py
@@ -160,7 +160,7 @@ class MatplotlibVisualizer:
                 "x": torch.zeros_like(global_agent_states.goal_x),
                 "y": torch.zeros_like(global_agent_states.goal_y),
             }
-            # Generate reverse offsets for controlled agents
+            # Generate random offsets for controlled agents
             for env_idx in env_indices:
                 controlled_mask = self.controlled_agent_mask[env_idx]
 
@@ -271,6 +271,19 @@ class MatplotlibVisualizer:
             figs.append(fig)  # Add the new figure
             plt.close(fig)  # Close the figure to prevent carryover
 
+            # Render the pre-cached road graph for the current environment
+            # cached_roadgraph_array = utils.bg_img_from_fig(self.cached_roadgraphs[env_idx])
+            # ax.imshow(
+            #     cached_roadgraph_array,
+            #     origin="upper",
+            #     extent=(-100, 100, -100, 100),  # Stretch to full plot
+            #     zorder=0,  # Draw as background
+            # )
+
+            # Explicitly set the axis limits to match your coordinates
+            # cached_ax.set_xlim(-100, 100)
+            # cached_ax.set_ylim(-100, 100)
+
             # Get control mask and omit out-of-bound agents (dead agents)
             controlled = self.controlled_agent_mask[env_idx, :]
             controlled_live = controlled & (
@@ -281,7 +294,8 @@ class MatplotlibVisualizer:
             is_collided = (
                 agent_infos[env_idx, :, 1:3].sum(axis=1) == 1
             ) & controlled_live
-            is_ok = ~is_offroad & ~is_collided & controlled_live
+
+            is_ok = ~is_offroad & ~is_collided & controlled_live ## make mask with policies
 
             # Draw the road graph
             self._plot_roadgraph(
@@ -303,6 +317,11 @@ class MatplotlibVisualizer:
 
             
             # Draw the agents
+            if policy_masks:
+                policy_mask = policy_masks[idx] 
+            else:
+                policy_mask = None
+            
             self._plot_filtered_agent_bounding_boxes(
                 ax=ax,
                 env_idx=env_idx,
@@ -319,8 +338,6 @@ class MatplotlibVisualizer:
             )
 
             if agent_positions is not None:
-                # First calculate the maximum valid trajectory length across all agents for this env_idx
-                max_valid_length = 0
                 for agent_idx in range(agent_positions.shape[1]):
                     if controlled_live[agent_idx]:
                         trajectory = agent_positions[
@@ -357,6 +374,7 @@ class MatplotlibVisualizer:
                         valid_trajectory = trajectory[valid_mask]
 
                         if len(valid_trajectory) > 1:
+                            # Convert to numpy and ensure correct shape
                             points = valid_trajectory.cpu().numpy()
 
                             if self.render_3d:
@@ -468,9 +486,10 @@ class MatplotlibVisualizer:
 
         for fig in figs:
             fig.tight_layout(pad=2, rect=[0.00, 0.00, 0.9, 1])
-            fig.tight_layout(pad=2, rect=[0.00, 0.00, 0.9, 1])
+
 
         return figs
+
 
     def _plot_log_replay_trajectory(
         self,
@@ -1237,51 +1256,74 @@ class MatplotlibVisualizer:
                 bboxes_controlled_ok, AGENT_COLOR_BY_POLICY,by_policy=True
             )
 
-        # Plot log replay agents
-        log_replay = (
-            response_type.static[env_idx, :] | response_type.moving[env_idx, :]
-        ) & ~self.controlled_agent_mask[env_idx, :]
+            # Plot log replay agents
+            log_replay = (
+                response_type.static[env_idx, :] | response_type.moving[env_idx, :]
+            ) & ~self.controlled_agent_mask[env_idx, :]
 
-        pos_x = agent_states.pos_x[env_idx, log_replay]
-        pos_y = agent_states.pos_y[env_idx, log_replay]
-        rotation_angle = agent_states.rotation_angle[env_idx, log_replay]
-        vehicle_length = agent_states.vehicle_length[env_idx, log_replay]
-        vehicle_width = agent_states.vehicle_width[env_idx, log_replay]
+            pos_x = agent_states.pos_x[env_idx, log_replay]
+            pos_y = agent_states.pos_y[env_idx, log_replay]
+            rotation_angle = agent_states.rotation_angle[env_idx, log_replay]
+            vehicle_length = agent_states.vehicle_length[env_idx, log_replay]
+            vehicle_width = agent_states.vehicle_width[env_idx, log_replay]
 
-        valid_mask = (
-            (torch.abs(pos_x) < OUT_OF_BOUNDS)
-            & (torch.abs(pos_y) < OUT_OF_BOUNDS)
-            & (
-                (vehicle_length > 0.5)
-                & (vehicle_length < 15)
-                & (vehicle_width > 0.5)
-                & (vehicle_width < 15)
+            valid_mask = (
+                (torch.abs(pos_x) < OUT_OF_BOUNDS)
+                & (torch.abs(pos_y) < OUT_OF_BOUNDS)
+                & (
+                    (vehicle_length > 0.5)
+                    & (vehicle_length < 15)
+                    & (vehicle_width > 0.5)
+                    & (vehicle_width < 15)
+                )
             )
-        )
 
-        bboxes_static = np.stack(
-            (
-                pos_x[valid_mask].numpy(),
-                pos_y[valid_mask].numpy(),
-                vehicle_length[valid_mask].numpy(),
-                vehicle_width[valid_mask].numpy(),
-                rotation_angle[valid_mask].numpy(),
-            ),
-            axis=1,
-        )
-        if policy_mask:
+            bboxes_static = np.stack(
+                (
+                    pos_x[valid_mask].numpy(),
+                    pos_y[valid_mask].numpy(),
+                    vehicle_length[valid_mask].numpy(),
+                    vehicle_width[valid_mask].numpy(),
+                    rotation_angle[valid_mask].numpy(),
+                ),
+                axis=1,
+            )
+
             if self.render_3d:
                 plot_agent_group_3d(bboxes_static, AGENT_COLOR_BY_STATE["log_replay"])
             else:
                 plot_agent_group_2d(bboxes_static, AGENT_COLOR_BY_STATE["log_replay"])
-                if self.render_3d:
-                    plot_agent_group_3d(
-                    bboxes_static, AGENT_COLOR_BY_STATE["log_replay"]
-                )
-                else:
-                    plot_agent_group_2d(
-                    bboxes_static, AGENT_COLOR_BY_STATE["log_replay"]
-                                  )
+        else:
+
+            bboxes = []
+
+            for policy_name,mask in policy_mask.items():
+          
+                bboxes_controlled_ok = np.stack(
+                    (
+                        agent_states.pos_x[env_idx, mask].numpy(),
+                        agent_states.pos_y[env_idx, mask].numpy(),
+                        agent_states.vehicle_length[env_idx, mask].numpy(),
+                        agent_states.vehicle_width[env_idx, mask].numpy(),
+                        agent_states.rotation_angle[env_idx, mask].numpy(),
+                    ),
+                    axis=1,
+                    )
+                bboxes.append(bboxes_controlled_ok)
+
+            
+            utils.plot_numpy_bounding_boxes_multiple_policy(
+                ax=ax,
+                bboxes_s=bboxes,
+                colors=AGENT_COLOR_BY_POLICY,#AGENT_COLOR_BY_STATE["ok"],
+                alpha=alpha,
+                line_width_scale=line_width_scale,
+                as_center_pts=as_center_pts,
+                label=label,
+            )
+
+
+
 
     def _plot_expert_trajectories(
         self,
