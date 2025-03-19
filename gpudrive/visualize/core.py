ccd7d3dc0e57c9e1ee69eb44e775e77b1f0eb183
Charlie Molony <73535968+charliemolony@users.noreply.github.com>
2025-03-19 15:19:59 -0400

    Fix visualizer bug (#386)
    * fixing bug and moving multi policy rollout
    
    * resolving comments
    
    * resolving comments

--- gpudrive/visualize/core.py
+++ gpudrive/visualize/core.py
@@ -1223,11 +1223,11 @@ class MatplotlibVisualizer:
             )
             if self.render_3d:
                 plot_agent_group_3d(
-                    bboxes_controlled_collided, AGENT_COLOR_BY_STATE["ok"]
+                    bboxes_controlled_ok, AGENT_COLOR_BY_STATE["ok"]
                 )
             else:
                 plot_agent_group_2d(
-                    bboxes_controlled_collided, AGENT_COLOR_BY_STATE["ok"]
+                    bboxes_controlled_ok, AGENT_COLOR_BY_STATE["ok"]
                 )
 
         else:
