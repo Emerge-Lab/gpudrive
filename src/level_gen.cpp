cc5e33b3a986baf7247ef4eb34ed42ecb1d2bab8
kevin <kevinwinston184@gmail.com>
2025-04-22 18:45:44 -0400

    agent init fix?

--- src/level_gen.cpp
+++ src/level_gen.cpp
@@ -101,10 +101,15 @@ static inline void populateExpertTrajectory(Engine &ctx, const Entity &agent, co
 
 static inline bool isAgentStatic(Engine &ctx, Entity agent) {
     auto agent_iface = ctx.get<AgentInterfaceEntity>(agent).e;
-    
-   // Static agents are those that are not tracks to predict
-    if (ctx.data().params.readFromTracksToPredict and ctx.get<MetaData>(agent_iface).isTrackToPredict != -1) {
-        return false;
+
+    // Static agents are those that are not tracks to predict
+    if (ctx.data().params.readFromTracksToPredict) {
+        if (ctx.get<MetaData>(agent_iface).isTrackToPredict == -1) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
     
     // Original logic for other initialization modes
