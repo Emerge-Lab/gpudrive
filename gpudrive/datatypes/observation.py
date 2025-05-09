4e104403da99e4b694e32c4717a4ff2c9837e159
Eugene Vinitsky <eugenevinitsky@users.noreply.github.com>
2025-05-09 16:27:22 -0400

    model looks kinda okay? (#439)
    * model looks kinda okay?
    
    * think we added the steer angle correctly to the env
    
    * Small bug fix
    
    ---------
    
    Co-authored-by: Daphne Cornelisse <cor.daphne@gmail.com>

--- gpudrive/datatypes/observation.py
+++ gpudrive/datatypes/observation.py
@@ -40,6 +40,7 @@ class LocalEgoState:
             self.is_collided = self_obs_tensor[:, 6]
             self.is_goal_reached = self_obs_tensor[:, 7]
             self.id = self_obs_tensor[:, 8]
+            self.steer_angle = self_obs_tensor[:, 9]
         else:
             self.speed = self_obs_tensor[:, :, 0]
             self.vehicle_length = self_obs_tensor[:, :, 1] * AGENT_SCALE
@@ -50,6 +51,7 @@ class LocalEgoState:
             self.is_collided = self_obs_tensor[:, :, 6]
             self.is_goal_reached = self_obs_tensor[:, :, 7]
             self.id = self_obs_tensor[:, :, 8]
+            self.steer_angle = self_obs_tensor[:, :, 9]
 
     @classmethod
     def from_tensor(
@@ -87,6 +89,7 @@ class LocalEgoState:
             min_val=constants.MIN_REL_GOAL_COORD,
             max_val=constants.MAX_REL_GOAL_COORD,
         )
+        self.steer_angle /= (torch.pi / 3)
 
     @property
     def shape(self) -> tuple[int, ...]:
