6e9856328f80640f7fccef346b74f099f0bbfbad
Daphne Cornelisse <cor.daphne@gmail.com>
2025-05-11 15:59:34 -0400

    Give agent a bit more road graph information

--- src/consts.hpp
+++ src/consts.hpp
@@ -10,7 +10,7 @@ namespace consts {
 
 inline constexpr madrona::CountT kMaxAgentCount = 32;
 inline constexpr madrona::CountT kMaxRoadEntityCount = 10000;
-inline constexpr madrona::CountT kMaxAgentMapObservationsCount = 128;
+inline constexpr madrona::CountT kMaxAgentMapObservationsCount = 186;
 
 inline constexpr bool useEstimatedYaw = true;
 
