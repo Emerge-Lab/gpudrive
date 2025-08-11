1eeb07d21b6d8be0b309a5a15e78fee0429beb36
mpragnay <mpragnay@gmail.com>
2025-08-11 12:06:47 -0400

    Added memory profiling

--- profiling/profile_madrona_gpudrive.py
+++ profiling/profile_madrona_gpudrive.py
@@ -12,6 +12,7 @@ from tqdm import tqdm
 from multiprocessing import Process, Queue
 
 import madrona_gpudrive
+import threading
 
 MAX_CONT_AGENTS = 64
 EPISODE_LENGTH = 80
@@ -58,6 +59,7 @@ def make_sim(
         params.maxNumControlledAgents = MAX_CONT_AGENTS
     elif actor_type == "expert-actor":
         params.maxNumControlledAgents = 0
+    print("Initialized all parameters")
     sim = madrona_gpudrive.SimManager(
         exec_mode=madrona_gpudrive.madrona.ExecMode.CPU
         if device == "cpu"
@@ -66,19 +68,19 @@ def make_sim(
         scenes=scenes,
         params=params,
     )
-    print("Sim Manager Parameters:")
-    for attr in dir(params):
-        print(f'ATTRIBUTE: {attr}')
-        if not attr.startswith("__"):
-            value = getattr(params, attr)
-            print(f"{attr:20}: {value}")
-            if attr == "rewardParams":
-                print("Reward parameters:")
-                reward_params = getattr(params, attr)
-                for attr2 in dir(reward_params):
-                    if not attr2.startswith("__"):
-                        value2 = getattr(reward_params, attr2)
-                        print(f"    {attr2:18}: {value2}")
+    # print("Sim Manager Parameters:")
+    # for attr in dir(params):
+    #     print(f'ATTRIBUTE: {attr}')
+    #     if not attr.startswith("__"):
+    #         value = getattr(params, attr)
+    #         print(f"{attr:20}: {value}")
+    #         if attr == "rewardParams":
+    #             print("Reward parameters:")
+    #             reward_params = getattr(params, attr)
+    #             for attr2 in dir(reward_params):
+    #                 if not attr2.startswith("__"):
+    #                     value2 = getattr(reward_params, attr2)
+    #                     print(f"    {attr2:18}: {value2}")
     return sim
 
 def warmup(sim, batch_size):
@@ -119,7 +121,7 @@ def run_speed_bench(
         obs_type=obs_type,
         num_worlds=batch_size,
     )
-
+    print(f"Created simulator with {batch_size} worlds")
     # Warmup
     warmup(sim, batch_size)
 
@@ -130,10 +132,10 @@ def run_speed_bench(
         # Get action tensor
         action_tensor = sim.action_tensor().to_torch()
 
-        print(f"Action tensor has a shape of (num_worlds, max_num_agents_in_scene, 3): {action_tensor.shape}")
+        # print(f"Action tensor has a shape of (num_worlds, max_num_agents_in_scene, 3): {action_tensor.shape}")
 
         rand_actions = torch.randint(
-            0, 9, size=(action_tensor.shape[0], max_num_objects, 10)
+            0, 9, size=action_tensor.shape
         )
 
         # Step
@@ -163,7 +165,7 @@ def run_speed_bench(
         total_agent_frames += (
             sim.controlled_state_tensor().to_torch().flatten().shape[0]
         )
-
+    print(f'Total step time: {total_step_time:.5f} seconds')
     # Store valid objects per scene
     valid_obj_dist = (
         (sim.controlled_state_tensor().to_torch() == 1)
@@ -203,8 +205,32 @@ def run_simulation(
     obs_type="classic",
 ):
     q = Queue()
+    mem_q = Queue()
+
+    def run_bench_and_monitor_mem(*args):
+        max_mem = [0]
+        def monitor_mem():
+            gpu_id = 0
+            while not stop_event.is_set():
+                mem = GPUtil.getGPUs()[gpu_id].memoryUsed
+                if mem > max_mem[0]:
+                    max_mem[0] = mem
+
+        stop_event = threading.Event()
+        monitor_thread = threading.Thread(target=monitor_mem)
+        monitor_thread.start()
+        run_speed_bench(*args)
+        stop_event.set()
+        monitor_thread.join()
+        # Check if thread is stopped
+        if not monitor_thread.is_alive():
+            print("Memory monitor thread has stopped.")
+        else:
+            print("Memory monitor thread is still running!")
+        mem_q.put(max_mem[0])
+
     p = Process(
-        target=run_speed_bench,
+        target=run_bench_and_monitor_mem,
         args=(
             batch_size,
             max_num_objects,
@@ -217,24 +243,30 @@ def run_simulation(
             q,
         ),
     )
+    print("Starting process for benchmark...")
     p.start()
     p.join()  # Wait for the process to finish
-    return q.get()
+    result = q.get()
+    max_gpu_mem = mem_q.get()
+    # Return max GPU memory used along with previous results
+    print(f"Max GPU memory used: {max_gpu_mem} MB")
+    return (*result, max_gpu_mem)
 
 
 if __name__ == "__main__":
 
     DATA_FOLDER = "data/processed/examples"
     # BATCH_SIZE_LIST = [2, 4, 8, 16, 32, 64, 128, 256, 512]
-    BATCH_SIZE_LIST = [2]
+    BATCH_SIZE_LIST = [8]
     ACTOR_TYPE = "random" # "expert_actor"
     DEVICE = "cuda"
     DATASET_INIT = "first_n" # or "random"
     OBS_TYPE = "lidar" # or "lidar"
 
     scenes = [os.path.join(DATA_FOLDER, scene) for scene in os.listdir(DATA_FOLDER)]
+    print(f"Scenes: {scenes}")
     # take first 100 scenes
-    scenes = scenes[:128]
+    # scenes = scenes[:128]
 
     # Get device info
     device_name = GPUtil.getGPUs()[0].name
@@ -248,13 +280,23 @@ if __name__ == "__main__":
     tot_valid_frames = np.zeros(len(BATCH_SIZE_LIST))
     tot_agent_frames = np.zeros(len(BATCH_SIZE_LIST))
     valid_obj_dist_lst = []
+    max_gpu_mem = np.zeros(len(BATCH_SIZE_LIST))
 
     pbar = tqdm(BATCH_SIZE_LIST, colour="green")
     for idx, batch_size in enumerate(pbar):
         if DATASET_INIT == "random":
             sampled_scenes = random.sample(scenes, batch_size)
         elif DATASET_INIT == "first_n":
-            sampled_scenes = scenes[:batch_size]
+            if batch_size > len(scenes):
+                # Repeat scenes as needed to reach batch_size
+                repeats = batch_size // len(scenes)
+                remainder = batch_size % len(scenes)
+                if remainder > 0:
+                    sampled_scenes = scenes * repeats + scenes[:remainder]
+                else:
+                    sampled_scenes = scenes * repeats
+            else:
+                sampled_scenes = scenes[:batch_size]
         pbar.set_description(
             f"Profiling gpudrive with batch size {batch_size} using {ACTOR_TYPE}"
         )
@@ -277,6 +319,7 @@ if __name__ == "__main__":
             tot_valid_frames[idx],
             tot_agent_frames[idx],
             valid_obj_dist,
+            max_gpu_mem[idx],
         ) = result
 
         valid_obj_dist_lst.append(valid_obj_dist)
@@ -294,6 +337,7 @@ if __name__ == "__main__":
             "batch_size (num envs)": BATCH_SIZE_LIST,
             "avg_time_per_reset (ms)": (tot_reset_times / tot_resets) * 1000,
             "avg_time_per_step (ms)": (tot_step_times / tot_steps) * 1000,
+            "max_gpu_mem (MB)": max_gpu_mem,
             "all_agent_fps (throughput)": tot_agent_frames / tot_step_times,
             "val_agent_fps (goodput)": tot_valid_frames / tot_step_times,
             "total_steps": tot_steps,
